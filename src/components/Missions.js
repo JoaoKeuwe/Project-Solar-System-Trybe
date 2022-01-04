30da3fd4e56338fc18e221b7379f9c51af6787e9
João Keuwe <joaov2872@hotmail.com>
2022-01-04 15:02:29 -0300

    requisito 07

---
+++ src/components/Missions.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Missions extends React.Component {
+  render() {
+    return (
+      <div data-testid="missions">
+        <p />
+      </div>
+    );
+  }
+}
+export default Missions;
